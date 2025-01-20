0ca130545c266976f2118d2187a6ad8d55c82bee
ChamkhiAnas <mahmoudbennouna@gmail.com>
2025-01-20 17:29:53 +0100

    adding biderectionelle relation between tags and posts

--- src/tags/schemas/tag.schema.ts
+++ src/tags/schemas/tag.schema.ts
@@ -1,9 +1,13 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Types } from "mongoose";
 
 @Schema()
 export class Tag {
     @Prop({required:true})
     name:string
+
+    @Prop({type:[{type:Types.ObjectId,ref:'Post'}]})
+    posts:Types.ObjectId[];
 }
 
 export const TagSchema = SchemaFactory.createForClass(Tag)
